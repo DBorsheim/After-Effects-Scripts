4e7aca0d532f912bc3fd29d68ce6106b822f931d
DBorsheim <33246513+DBorsheim@users.noreply.github.com>
2021-05-18 11:08:30 -0500

    Rename 15 Frames Back.jsx to AE Scripts/15 Frames Back.jsx

--- 15 Frames Back.jsx
+++
@@ -1,2 +0,0 @@
-amt = (1 / app.project.activeItem.frameRate) * 5;
-app.project.activeItem.time = app.project.activeItem.time - amt;
---
+++ AE Scripts/15 Frames Back.jsx
@@ -0,0 +1,2 @@
+amt = (1 / app.project.activeItem.frameRate) * 5;
+app.project.activeItem.time = app.project.activeItem.time - amt;
